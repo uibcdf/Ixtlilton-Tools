77fa2d18d13520ae223e4b1dcf2f303ee97b2569
Diego Prada <prada.gracia@gmail.com>
2021-11-18 22:32:51 -0600

    Adding tests and fixing requirements

---
+++ ixtlilton/tests/test_import.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the ixtlilton package.
+"""
+
+# Import package, test suite, and other packages as needed
+import ixtlilton
+import pytest
+import sys
+
+def test_ixtlilton_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "ixtlilton" in sys.modules
